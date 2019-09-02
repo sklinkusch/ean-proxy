35a1ab1691587eb5070c6417929bb02cb5b85fd4
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-02 21:20:38 +0200

    preliminary build with test access

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = async (req, res) => {
   const { query } = parse(req.url);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', "text/plain; charset='UTF-8'; charset='utf-8'");
-  const requestUrl = `http://opengtindb.org?queryid=400000000&cmd=query&ean=${query}`;
+  const requestUrl = `http://opengtindb.org?queryid=${process.env.API_KEY}&cmd=query&ean=${query}`;
   fetch(requestUrl)
     .then(response => response.text())
     .then(data => {
