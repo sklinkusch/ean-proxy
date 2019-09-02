94012f651fd1fa20347cda4b8a0e3550383cce25
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-02 21:15:19 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+require('isomorphic-fetch');
+require('now-env');
+
+const { parse } = require('url');
+const { send } = require('micro');
+
+module.exports = async (req, res) => {
+  const { query } = parse(req.url);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', "text/plain; charset='UTF-8'; charset='utf-8'");
+  const requestUrl = `http://opengtindb.org?queryid=400000000&cmd=query&ean=${query}`;
+  fetch(requestUrl)
+    .then(response => response.text())
+    .then(data => {
+      send(res, 200, data);
+    })
+    .catch(error => {
+      send(res, 500, error);
+    });
+};
